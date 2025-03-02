d88778e5514196d91ecd5c9992c10c525d17233c
RuzilKutliakhmetov <115926681+RuzilKutliakhmetov@users.noreply.github.com>
2025-03-02 10:35:44 +0500

    Добавлена сущность для Отзывов

--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
-import { ReviewService } from './review.service';
-import { ReviewController } from './review.controller';
+import { Module } from '@nestjs/common'
+import { PrismaService } from 'src/prisma.service'
+import { ReviewController } from './review.controller'
+import { ReviewService } from './review.service'
 
 @Module({
-  controllers: [ReviewController],
-  providers: [ReviewService],
+	controllers: [ReviewController],
+	providers: [ReviewService, PrismaService]
 })
 export class ReviewModule {}
